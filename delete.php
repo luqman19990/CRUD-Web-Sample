0a576fd1f2a9ab9828379a53bdcbeb7ab1486fb5
luqman19990 <82139613+luqman19990@users.noreply.github.com>
2024-06-01 20:55:54 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,18 @@
+<?php
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "myshop";
+    
+    $connection = new mysqli($servername, $username, $password, $database);
+    
+    $sql = "DELETE FROM clients WHERE id=$id";
+    $result = $connection->query($sql);
+}
+header("location: index.php");
+exit;
+?>
 
